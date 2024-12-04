6ce35b135bea34373ad36d1d6c4cf2d529edb32a
zhiyuan yan <yanzhiyuan2019@gmail.com>
2024-12-04 14:48:35 -0800

    some change

--- gen.py
+++ gen.py
@@ -27,22 +27,27 @@ def gen_transitions(maxState: int, possible_moves: list[str]):
         res += gen_transitions_helper(move, maxState, possible_moves)
     return res
 
-def gen_main_fun(init_state: int, init_action: int, true_actions: list[int], possible_moves: list[str], possible_moves_vars: str):
+def gen_main_fun(init_action: int, true_actions: list[int], possible_moves: list[str], possible_moves_vars: str):
+    # main func declaration and initial states
     main_code = "harness void main() {\n"
-    main_code += f"int[t] true_actions = {{{','.join(map(str, true_actions))}}};\nint init_state = {init_state};\nint init_action = {init_action};\n"
-    #int l = left({init_state}, {init_action});
-    #int r = right(1, {init_action}); # 
+    main_code += f"int init_action = {init_action};\n"
+    main_code += f"int[t] true_actions = {{{','.join(map(str, true_actions))}}};\n"
     
+    # generate the initial state for each movement
+    for i, a in enumerate(possible_moves_vars):
+        main_code += f"int init_state_{a} = {0 if i == init_action else 1};\n"
+
+    # assert statement
     assert_statements = []
     for i, (a, action) in enumerate(zip(possible_moves_vars, possible_moves)):
-        main_code += f"int {a} = {action}({init_state+i}, init_action);\n"
+        main_code += f"int {a} = {action}(init_state_{a}, init_action);\n"
         assert_statements.append(f"({a} == 0 && " + " && ".join([f"{a1} != 0" for a1 in possible_moves_vars if a1 != a]) + ")")
 
-      
     assert_statements = "assert " + " || ".join(assert_statements) + ";\n"
     main_code += assert_statements
+
+    # if statement for next
     main_code += "int next;\n"
-    
     next_state_code = ""
     for i, a in enumerate(possible_moves_vars):
       if i == 0: next_state_code += f"if ({a} == 0) {{\n\tnext = {i};\n"
@@ -50,8 +55,11 @@ def gen_main_fun(init_state: int, init_action: int, true_actions: list[int], pos
     next_state_code += f"}}"
     
     main_code += next_state_code
-    main_code += "\nassert (next == init_action);\n\nfor (int i=0; i<t; i++) {\n"
-    
+    #main_code += "\nassert (next == init_action);\n\n"
+    main_code += "\nassert (next == true_action[0]);\n\n"
+
+    # true action 1 to t for loop
+    main_code += "for (int i=1; i<t; i++) {\n"
     for a, action in zip(possible_moves_vars, possible_moves):
       main_code += f"\t{a} = {action}({a}, next);\n"
     
@@ -65,14 +73,14 @@ def main():
     true_actions = [0,1,2,3,0,3,2,3]
     time_steps = len(true_actions)
     
-    init_state = 0
+    # init_state = 0
     init_action = 1
     
     possible_moves = ["move_left", "move_right", "move_up", "move_down"]
     possible_moves_vars = 'lrud'
     
     transition_code = gen_transitions(max_states, possible_moves)
-    main_code = gen_main_fun(init_state, init_action, true_actions, possible_moves, possible_moves_vars)
+    main_code = gen_main_fun(init_action, true_actions, possible_moves, possible_moves_vars)
     
     print(f"{header(max_states, time_steps)}\n{transition_code}\n{main_code}")
     
